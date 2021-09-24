199bb89bb7fd673458fbbe88cc5cbc2a4eb2f091
Isabel RUbim <isabel_rubim@hotmail.com.br>
2021-09-23 21:59:04 -0300

    add integration with graphcms

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://my-trips.isabelrubim.com.br',
+    site_name: 'My Trips'
+  },
+  twitter: {
+    handle: '@isabelrubim',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+};
